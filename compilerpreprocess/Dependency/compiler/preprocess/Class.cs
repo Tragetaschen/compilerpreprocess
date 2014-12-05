bc1cb719ebf0205a6d23c2ea860b7e348ac1df61
Kai Ruhnau <kai.ruhnau@target-sg.com>
2014-12-05 17:04:38 +0100

    Added the repro

---
+++ compilerpreprocess/Dependency/compiler/preprocess/Class.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Framework.Runtime;
+using System;
+
+namespace Dependency.compiler.preprocess
+{
+    public class Class : ICompileModule
+    {
+        public Class(IApplicationEnvironment applicationEnvironment)
+        {
+            Console.WriteLine("Name: \{applicationEnvironment.ApplicationName}");
+        }
+        public void BeforeCompile(IBeforeCompileContext context)
+        {
+        }
+
+        public void AfterCompile(IAfterCompileContext context)
+        {
+        }
+    }
+}
